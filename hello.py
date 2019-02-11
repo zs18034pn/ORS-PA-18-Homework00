5511458a5be1f9b63a60c023419921d1057b7a03
Zoja Šćekić <zoja.scekic@udg.edu.me>
2019-02-11 15:02:37 +0100

    Add rad

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
+korisnik_uneo_prvi = input("Unesite prvi broj: ")
+korisnik_uneo_drugi = input("Unesite drugi broj: ")
 
-def greet(ime , prezime):
-    print("Hello there" + " " + ime +" " + prezime + "!")
-greet("Zoja", "Šćekić")
+if (not  korisnik_uneo_prvi.isnumeric()) or (not korisnik_uneo_drugi.isnumeric()) :
+    print("Niste uneli dobre brojeve")
+    quit()
+
+
+korisnik_broj_prvi = int(korisnik_uneo_prvi)
+korisnik_broj_drugi = int(korisnik_uneo_drugi)
+
+zbir = korisnik_broj_prvi + korisnik_broj_drugi
+print(zbir)
+
